6e8e5f81fc3da7b9a6dcca2e1b5ac01a3a1aeacd
Oleg898333 <barinov1455@gmail.com>
2025-05-02 17:16:13 +0500

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,39 @@
+# напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
+from instr import *
+from second_win import *
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def  initUI(self):
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.button = QPushButton(txt_next)
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.hello_text)
+        self.v_line.addWidget(self.instruction)
+        self.v_line.addWidget(self.button, alignment=Qt.AlignCenter)
+        self.setLayout(self.v_line)
+
+    def  connects(self):
+        self.button.clicked.connect(self.next_click)
+
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
+
