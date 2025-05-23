860ca256cf3f682a4793f1b5f794791224f04500
Мария <161601376+maryvash@users.noreply.github.com>
2025-05-23 18:19:10 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -18,11 +18,14 @@ class MainWin(QWidget):
         self.move(win_x, win_y)
     def  initUI(self):
         self.hello_text = QLabel(txt_hello)
+        self.hello_text.setStyleSheet("color: #519600; font-family: Times New Roman; font-size: 20px; margin-bottom:0px")
         self.instruction = QLabel(txt_instruction)
+        self.instruction.setStyleSheet("color: #519600; font-family: Times New Roman; font-size: 15px; text-align: center;")
         self.button = QPushButton(txt_next)
+        self.button.setStyleSheet("color: #519600; font-family: Times New Roman; font-size: 15px; background: white")
         self.v_line = QVBoxLayout()
-        self.v_line.addWidget(self.hello_text)
-        self.v_line.addWidget(self.instruction)
+        self.v_line.addWidget(self.hello_text, alignment=Qt.AlignCenter)
+        self.v_line.addWidget(self.instruction, alignment=Qt.AlignCenter)
         self.v_line.addWidget(self.button, alignment=Qt.AlignCenter)
         self.setLayout(self.v_line)
 
@@ -32,10 +35,32 @@ class MainWin(QWidget):
     def next_click(self):
         self.hide()
         self.tw = TestWin()
+        self.tw.setStyleSheet("background-color: #B5F36D; color: #519600; font-family: Times New Roman; font-size: 18px; margin-bottom:0px")
 
-
+STYLE = '''
+QPushButton {
+    outline: none;
+    border: 1px solid #519600;
+    padding: 5px;
+    border-radius: 5px;
+    text-transform: uppercase;
+    background: white;
+    color: #519600;
+    font-family: Times New Roman;
+    font-size: 15px;
+}
+QLineEdit {
+    outline: none;
+    border: 1px solid #519600;
+    padding: 5px;
+    border-radius: 5px;
+}
+}
+'''
 app = QApplication([])
+app.setStyleSheet(STYLE)
 mw = MainWin()
+mw.setStyleSheet("background-color: #B5F36D;")
 app.exec_()
  
 
